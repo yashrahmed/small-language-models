ece5deb55e30e476d64fe61e07af67cc11507a1e
yashrahmed <yashrahmed@gmail.com>
2025-11-02 11:28:13 -0600

    Build a basic tokenizer with an encoder and a decoder + vocab builder

---
+++ build-llm-from-scratch/llm_components/__init__.py
@@ -0,0 +1,6 @@
+from .components import (
+    SimpleTokenizer,
+    VocabBuilder,
+    END_OF_TEXT_TOKEN,
+    UNK_TOKEN
+)
--- build-llm-from-scratch/test.py
+++ build-llm-from-scratch/test.py
@@ -9,10 +9,27 @@ def tokenize(text):
     tokens = re.split(r'([,.:;?_!\"()\']|--|\s)', text) # Use capture groups for retaining delimiters.
     return [token.strip() for token in tokens if token.strip()]
 
+def build_vocab(tokens):
+    sorted_tokens = sorted(set(tokens))
+    vocab = {}
+    for i, token in enumerate(sorted_tokens):
+        vocab[token]= i
+    return vocab
     
 
 if __name__ == '__main__':
     text = load_text()
-    tokens = tokenize(text)
-    print(len(tokens))
-    print(',' in tokens)
+    from .llm_components import VocabBuilder, SimpleTokenizer, END_OF_TEXT_TOKEN
+    # tokens = tokenize(text)
+    # print(tokens[:50])
+    v_builder = VocabBuilder()
+    vocab = v_builder.build_vocab(text)
+    
+    tok = SimpleTokenizer(vocab)
+    sentence1 = "Hello! Do you like tea?"
+    sentence2 = "In the sunlit terraces of the palace."
+    sentence = f" {END_OF_TEXT_TOKEN} ".join([sentence1, sentence2])
+    tok_ids = tok.encode(sentence)
+    text = tok.decode(tok_ids)
+    print(sentence)
+    print(text)
