f25d8dee17df643b3cb4476484d28eb7a832196a
yashrahmed <yashrahmed@gmail.com>
2025-11-03 22:12:10 -0600

    Basic BPE demo using the tiktoken library.

--- build-llm-from-scratch/test.py
+++ build-llm-from-scratch/test.py
@@ -1,25 +1,12 @@
-import re
+from llm_components import VocabBuilder, SimpleTokenizer, END_OF_TEXT_TOKEN
+
 
 def load_text():
     with open('verdict.txt', 'r') as book:
         text = book.read()
         return text
 
-def tokenize(text):
-    tokens = re.split(r'([,.:;?_!\"()\']|--|\s)', text) # Use capture groups for retaining delimiters.
-    return [token.strip() for token in tokens if token.strip()]
-
-def build_vocab(tokens):
-    sorted_tokens = sorted(set(tokens))
-    vocab = {}
-    for i, token in enumerate(sorted_tokens):
-        vocab[token]= i
-    return vocab
-    
-
-if __name__ == '__main__':
-    text = load_text()
-    from .llm_components import VocabBuilder, SimpleTokenizer, END_OF_TEXT_TOKEN
+def test_custom_tokenizer():
     # tokens = tokenize(text)
     # print(tokens[:50])
     v_builder = VocabBuilder()
@@ -33,3 +20,28 @@ if __name__ == '__main__':
     text = tok.decode(tok_ids)
     print(sentence)
     print(text)
+    
+def test_tiktoken():
+    # Tikton library provides BPE tokenizer.
+    # This tokenizer does not use <|unk|> token.
+    # When an unknow word is encountered then, BPE splits it down into smaller chunks that are part of its vocabulary.
+    # It keeps going until it encounters individual characters.
+
+    # I would like to implement it.
+
+    import tiktoken
+    tokenizer = tiktoken.get_encoding("gpt2")
+    sentence1 = "Hello! Do you like tea?"
+    sentence2 = "In the sunlit terraces of the palace."
+    sentence = f"{END_OF_TEXT_TOKEN}".join([sentence1, sentence2])
+    print(sentence)
+    token_ids = tokenizer.encode(sentence, allowed_special={"<|endoftext|>"})
+    print(token_ids)
+    print(tokenizer.decode(token_ids))
+
+
+
+if __name__ == '__main__':
+    text = load_text()
+    test_tiktoken()
+    
