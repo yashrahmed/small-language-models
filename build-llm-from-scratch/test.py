a2bfd1a3f79e58edc2679ac7101a7b72eebd3121
yashrahmed <yashrahmed@gmail.com>
2025-11-19 17:00:16 -0600

    Setting up Causal multi headed attention with dropout

--- build-llm-from-scratch/test.py
+++ build-llm-from-scratch/test.py
@@ -278,66 +278,71 @@ def building_causal_multiheaded_attention():
             [0.05, 0.80, 0.55] # step
         ]
     )
+
+    class CausalMultiHeadedAttention(nn.Module):
+        def __init__(self, d_in, d_space, context_len, num_heads, drop_rate, bias_on=False):
+            super().__init__()
+            self.W_key = nn.Linear(d_in, d_space, bias_on)
+            self.W_query = nn.Linear(d_in, d_space, bias_on)
+            self.W_value = nn.Linear(d_in, d_space, bias_on)
+            self.W_out = nn.Linear(d_space, d_space)
+            self.register_buffer('mask', triu(ones(context_len, context_len), diagonal=1))
+            self.dropout = nn.Dropout(drop_rate)
+            self.head_dim = d_space // num_heads
+            self.num_heads = num_heads
+            self.d_space = d_space
+            assert (d_space % num_heads == 0)
+        
+        def forward(self, input_emb): 
+            batches, num_tokens, _ = input_emb.shape
+
+            key_op = self.W_key(inputs)
+            query_op = self.W_query(inputs)
+            value_op = self.W_value(inputs)
+
+            # <SliceStep> 
+            # This is requires to slice the combined Q,K,V matrices 
+            # into two independent slices;
+            # Multiplying in one-go doesn't work as this would cause the second slice of
+            # Q to interfere with the first slice of K etc.
+
+            key_op = key_op.view(batches, num_tokens, self.num_heads, self.head_dim)
+            query_op = query_op.view(batches, num_tokens, self.num_heads, self.head_dim)
+            value_op = value_op.view(batches, num_tokens, self.num_heads, self.head_dim)
+
+            # Tranpose dims 1/2 of 3 in order to multiple
+            # Pytorch uses the last two dims when performing a matmul().
+            key_op = key_op.transpose(1, 2)
+            query_op = query_op.transpose(1, 2)
+            value_op = value_op.transpose(1, 2)
+
+            attn_mat = key_op @ query_op.transpose(2, 3) # transpose along last 2 dims to align for matmul().
+            mask_mat = self.mask.bool()[:num_tokens, :num_tokens]
+            attn_mat.masked_fill_(mask_mat, -inf) # See broadcast rules.
+            d_k = self.head_dim
+            attn_mat = self.dropout(softmax(attn_mat / d_k ** 0.5, dim=-1))
+
+            context_vec = attn_mat @ value_op
+            # Put it back into a shape that matches the outputs of <SliceStep> and rebuild the contiguous view
+            context_vec = context_vec.transpose(1, 2)
+            context_vec = context_vec.contiguous().view(batches, num_tokens, self.d_space)
+
+
+            # Usually we would return after the previous op.
+            # But for multiheaded attention, one last matrix multiply is required.
+            # Think of it like a weighting sum layer for all the value vectors
+            # a.k.a. remixing cross head interactions.
+            return self.W_out(context_vec)
+
+    
     inputs = torch.stack((inputs,), dim=0)
     print(inputs.shape)
 
     manual_seed(123)
 
-    d_space = 4 # 2d X 2 heads.
-    bias_on = False
-    num_heads = 2
-
-    batches, num_tokens, d_in = inputs.shape
-    head_dim = d_space // num_heads
-
-    assert (d_space % num_heads == 0)
-
-    W_key = nn.Linear(d_in, d_space, bias_on)
-    W_query = nn.Linear(d_in, d_space, bias_on)
-    W_value = nn.Linear(d_in, d_space, bias_on)
-    W_out = nn.Linear(d_space, d_space)
-    drop_layer = nn.Dropout(0.5)
-
-    key_op = W_key(inputs)
-    query_op = W_query(inputs)
-    value_op = W_value(inputs)
-
-    key_op = key_op.view(batches, num_tokens, num_heads, head_dim)
-    query_op = query_op.view(batches, num_tokens, num_heads, head_dim)
-    value_op = value_op.view(batches, num_tokens, num_heads, head_dim)
-
-    print(key_op.shape)
-
-    key_op = key_op.transpose(1, 2)
-    query_op = query_op.transpose(1, 2)
-    value_op = value_op.transpose(1, 2)
-
-    print(key_op.shape)
-
-    attn_mat = key_op @ query_op.transpose(2, 3)
-    mask = triu(ones(num_tokens, num_tokens), diagonal=1)
-    attn_mat.masked_fill_(mask.bool(), -inf) # Perform causal masking
-
-    d_k = key_op.shape[-1]
-    attn_mat = softmax(attn_mat / d_k ** 0.5, dim=-1)
-    attn_mat = drop_layer(attn_mat)
-
-    print(attn_mat.shape)
-
-    context_vec = attn_mat @ value_op
-    print(context_vec.shape)
-
-    # Put it back into the shape of the orginal views.
-    context_vec = context_vec.transpose(1, 2)
-    print(context_vec.shape)
-
-    # Rebuild the contiguous view
-    context_vec = context_vec.contiguous().view(batches, num_tokens, d_space)
-    print(context_vec.shape)
-
-    # Use an output projection matrix.
-    context_vec = W_out(context_vec)
-
+    layer = CausalMultiHeadedAttention(3, 4, 6, 2, 0.5)
+    output = layer(inputs)
+    print(output.shape)
 
 if __name__ == '__main__':
     building_causal_multiheaded_attention()
